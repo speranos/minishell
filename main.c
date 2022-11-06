e4bdc98f4e28f7dd860a03038425afbd468313be
speranos <aoueldma@student.1337.ma>
2022-11-06 18:05:25 +0100

    enima

--- main.c
+++ main.c
@@ -22,55 +22,6 @@ int main(int ac, char **av, char **env)
     ft_intro();
 	while(1)
 	{
-        str = readline("ach_tama> ");
-        printf("str ====== %s\n", str);
+        str = readline("ach_tama>$ ");
 	}
-    // while(env[i])
-    // {
-    //     printf("HOla comista do\n");
-    //     printf("env == %s\n", env[1]);
-    //     i++;
-    // }
 }
-
-// #include <signal.h>
-// #include <stdio.h>
-// #include <readline/readline.h>
-// #include <readline/history.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// void handler(int signum)
-// {
-//     if (signum != SIGINT)
-//         return;
-//     printf("ctrl + c\n");
-//     rl_on_new_line();
-//     rl_replace_line("", 1);
-//     rl_redisplay();
-// }
-
-// int main(void)
-// {
-//     int ret = 0;
-//     char *line;
-
-//     signal(SIGINT, handler);
-//     while (1)
-//     {
-//         line = readline("input> ");
-//         if (line)
-//         {
-//             if (ret)
-//                 printf("output> %s\n", line);
-//             add_history(line);
-//             free(line);
-//             line = NULL;
-//         }
-//         else
-//         {
-//             printf("ctrl + d\n");
-//         }
-//     }
-//     return (0);
-// }
