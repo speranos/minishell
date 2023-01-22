b31a92bd15d4f6fadc147cb53b952e0723709f55
enimv <oueldmamaamine@gmail.com>
2023-01-22 21:28:36 +0100

    enima

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 
 NAME = minishell
 #CC = cc
-CFLAGS = -lreadline -Wall -Wextra -Werror -fsanitize=address#-L/Users/aoueldma/goinfre/homebrew/opt/readline/lib -I /Users/aoueldma/goinfre/homebrew/opt/readline/include -fsanitize=address
+CFLAGS = -lreadline -Wall -Wextra -Werror #-fsanitize=address#-L/Users/aoueldma/goinfre/homebrew/opt/readline/lib -I /Users/aoueldma/goinfre/homebrew/opt/readline/include -fsanitize=address
 SRCS = lexer/*.c syntax_check/*.c parser/*.c expand/*.c *.c execution/built/*.c execution/libft/*.c
 all : $(NAME)
 
 $(NAME) : $(SRCS)
+	@stty -echoctl
 	@cc $(SRCS) $(CFLAGS) -o $(NAME)
 	@echo YOUR ARE READY TO GO !!!
 clean :
--- main.c
+++ main.c
@@ -6,22 +6,11 @@
 /*   By: aoueldma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:43:05 by aoueldma          #+#    #+#             */
-/*   Updated: 2023/01/19 21:43:08 by aoueldma         ###   ########.fr       */
+/*   Updated: 2023/01/22 17:26:32 by aoueldma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	ft_args_check(int ac, char **av, char **env)
-{
-	(void)av;
-	(void)env;
-	if (ac != 1)
-	{
-		printf("arg...error!!!\nprogram...exit...byye!!!\n");
-		exit(0);
-	}
-}
 
 void	ft_intro(void)
 {
@@ -46,7 +35,7 @@ void    sig_handler(int signum)
     {
         write (1, "\n", 1);
     }
-    if (signum == SIGINT && g_var.sig == 2)
+    if (signum == SIGINT && g_var.                                                    == 2)
     {
         g_var.exit_code = 1;
         g_var.herdoc_flag = 0;
@@ -63,6 +52,12 @@ void    sig_handler(int signum)
 // 	rl_replace_line("", 0);
 // 	rl_redisplay();
 // }
+// void	sig_handler(int sig_init)
+// {
+// 	sig_init = 0;
+// 	exit(0);
+// }
+
 void	init_env(t_envir **env, t_envir **exp)
 {
 	env = NULL;
@@ -72,33 +67,48 @@ void	init_env(t_envir **env, t_envir **exp)
 	exp = malloc(sizeof(t_envir));
 }
 
+void	ft_args_check(int ac, char **env, t_envir **envir, t_envir **exp)
+{
+	if (ac != 1)
+	{
+		printf("arg...error!!!\nprogram...exit...byye!!!\n");
+		exit(0);
+	}
+	ft_intro();
+	init_env(envir, exp);
+	ft_copy_env(env, envir);
+	ft_copy_env(env, exp);
+	signal(SIGQUIT, SIG_IGN);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	char		*input;
 	t_parser	*data;
-	// t_envir		*envir;
-	// t_envir		*exp;
+	t_envir		*envir;
+	t_envir		*exp;
 
-	ft_args_check(ac, av, env);
-	ft_intro();
-	// init_env(&envir, &exp);
-	// ft_copy_env(env, &envir);
-	// ft_copy_env(env, &exp);
-	//signal(SIGINT, sig_handler);
-	//signal(SIGQUIT, SIG_IGN);
+	(void)av;
+	ft_args_check(ac, env, &envir, &exp);
+	signal(SIGQUIT, SIG_IGN);
+	// signal(SIGINT, sig_handler);
 	while (1)
 	{
 		input = readline("ach_tama>$ ");
-		if (*input != '\0')
+		if (input)
 		{
 			add_history(input);
 			data = ft_lexxx(input);
 			if (data != NULL)
 			{
-				//ft_execution(&envir, &exp, data);
-				ft_print(data);
+				ft_execution(&envir, &exp, data);
 				ft_free_parser(data);
 			}
+		}
+		else
+		{
+			write(2, "exit\n", 6);
+			exit(1);
 		}
 		free(input);
 	}
